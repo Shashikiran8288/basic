02bc4a0a360fc4131ad09790ab51c75aab8b61f5
“shashikiran.ananthakumar” <shashikiran8288@gmail.com>
2022-02-09 14:43:52 +0530

    listing with filters

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,5 +1,5 @@
 const Product = require('../models').Product;
-
+const { Op } = require("sequelize");
 
 
 const create = function(req, res){
@@ -16,11 +16,62 @@ const create = function(req, res){
       .catch(error => res.status(400).send(error.toString()));
 }
 
-const list = function (req, res){
-    return Product
-      .findAll()
-      .then(products => res.status(200).send(products))
-      .catch(error => res.status(400).send(error));
+const list = function (req, res) {
+  let limit = req.query.limit ? req.query.limit : 10,
+    offset = (req.query.offset ? req.query.offset : 0),
+    searchParams = [], attributes = [
+      ['id', 'productId'],
+      ['name', 'product'],
+      ['type', 'productType'], 'weight', 'guage', 'size', ['rate', 'price']
+    ];
+  let whereClause = {
+    where: {},
+    attributes: attributes,
+    offset: offset,
+    limit: limit
+  };
+
+  if (req.query.search) {
+
+    req.query.search = JSON.parse(req.query.search)
+
+    if (req.query.search.productId) searchParams.push({
+      'id': {
+        [Op.eq]: req.query.search.productId
+      }
+    });
+    if (req.query.search.product) searchParams.push({
+      'name': {
+        [Op.iLike]: '%' + req.query.search.product + '%'
+      }
+    });
+    if (req.query.search.productType) searchParams.push({
+      'type': {
+        [Op.iLike]: '%' + req.query.search.productType + '%'
+      }
+    });
+    if (req.query.search.guage) searchParams.push({
+      'guage': {
+        [Op.iLike]: '%' + req.query.search.guage + '%'
+      }
+    });
+    if (req.query.search.size) searchParams.push({
+      'size': {
+        [Op.iLike]: '%' + req.query.search.size + '%'
+      }
+    });
+
+    whereClause.where = ((searchParams.length > 1) ? {
+      [Op.or]: searchParams
+    } : searchParams[0]);
+
+  }
+
+
+  return Product
+    .findAndCountAll(whereClause)
+    .then(products => res.status(200).send(products))
+    .catch(error => res.status(400).send(error.toString()));
 }
 
 const findById = function (req, res){
